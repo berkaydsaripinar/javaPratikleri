5b7b16a1d2a81306d8270bdf382d721af2e8b206
Berkay Sarıpınar <saripinar.js@gmail.com>
2023-08-23 23:41:42 +0300

    Update Main.java

--- Daire_Alan/src/Main.java
+++ Daire_Alan/src/Main.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        float r,a;
-        double pi = 3.14;
+        double r,a;
+        float pi = 3.14F;
 
         System.out.print("Yarıçapını giriniz: ");
-        r = input.nextFloat();
+        r = input.nextDouble();
         System.out.print("Merkez açısının ölçüsünü giriniz: ");
-        a = input.nextFloat();
+        a = input.nextDouble();
 
         double area = (pi *(r*r) *a)/360;
         System.out.print("Dairenin alanı : " + area);
