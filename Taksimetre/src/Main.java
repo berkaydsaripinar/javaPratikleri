840eb6ea31cf9bb780ca7f6021c33de852a6347e
Berkay Sarıpınar <saripinar.js@gmail.com>
2023-08-23 20:38:35 +0300

    first commit

---
+++ Taksimetre/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+     int tmAcis = 10;
+     double km,tutar;
+      System.out.println("\nTaksimetre Açılma Ücreti 10 TL'dir");
+      System.out.println("\nİndibindi ücreti 20 TL'dir");
+      System.out.print("\nMesafeyi KM cinsinden yazınız:  ");
+      km = scanner.nextDouble();
+      tutar = tmAcis + (2.20 * km);
+      if (tutar <= 20){
+          System.out.println("\nÖdeme tutarı:  20 TL");
+      }else {
+          System.out.println("\n Ödeme Tutarı: " +tutar+" TL");
+      }
+
+    }
+}
